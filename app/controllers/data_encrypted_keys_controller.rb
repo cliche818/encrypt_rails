48901d05fe68a336765ba78f85344907bb4433ef
jeffli <jeff.chun.yinli@gmail.com>
2018-12-19 21:19:43 -0500

    added scenario in data encrypted keys status endpoint when there is a job queued

--- app/controllers/data_encrypted_keys_controller.rb
+++ app/controllers/data_encrypted_keys_controller.rb
@@ -1,5 +1,12 @@
 class DataEncryptedKeysController < ApplicationController
   def rotate_status
-    render json: {message: 'No key rotation queued or in progress'}.to_json
+
+    if Sidekiq::Queue.new.size > 0
+      message = 'Key rotation has been queued'
+    else
+      message = 'No key rotation queued or in progress'
+    end
+
+    render json: {message: message}.to_json
   end
 end
--- spec/controllers/data_encrypted_keys_controller_spec.rb
+++ spec/controllers/data_encrypted_keys_controller_spec.rb
@@ -11,5 +11,15 @@ RSpec.describe DataEncryptedKeysController, type: :controller do
       json_response = JSON.parse(response.body)
       expect(json_response).to eq('message' => 'No key rotation queued or in progress')
     end
+
+    it 'should return a json response for key rotation job has been queued' do
+      sidekiq_queue_object = [OpenStruct.new(klass: 'WorkerClass')]
+      allow(Sidekiq::Queue).to receive(:new).and_return(sidekiq_queue_object)
+
+      get :rotate_status
+
+      json_response = JSON.parse(response.body)
+      expect(json_response).to eq('message' => 'Key rotation has been queued')
+    end
   end
 end
