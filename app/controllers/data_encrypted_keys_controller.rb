043f4ce541a918ac8c5d6e3bc60df99c7ca48bf1
jeffli <jeff.chun.yinli@gmail.com>
2018-12-19 22:04:09 -0500

    added scenario in data encrypted keys status endpoint when there is a job in progress (assuming the job is in default and there is only default queue + only 1 possible job, need to fix this later to be more specific)

--- app/controllers/data_encrypted_keys_controller.rb
+++ app/controllers/data_encrypted_keys_controller.rb
@@ -1,12 +1,29 @@
+require 'pry'
+
 class DataEncryptedKeysController < ApplicationController
   def rotate_status
 
     if Sidekiq::Queue.new.size > 0
       message = 'Key rotation has been queued'
+    elsif key_rotation_job_in_progress?
+      message = 'Key rotation is in progress'
     else
       message = 'No key rotation queued or in progress'
     end
 
     render json: {message: message}.to_json
+  end
+
+  private
+
+  def key_rotation_job_in_progress?
+    workers = Sidekiq::Workers.new
+
+    result = false
+    workers.each do |_process_id, _thread_id, work|
+      result = true if work['payload']['queue'].present?
+    end
+
+    result
   end
 end
--- spec/controllers/data_encrypted_keys_controller_spec.rb
+++ spec/controllers/data_encrypted_keys_controller_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.describe DataEncryptedKeysController, type: :controller do
   describe '#rotate_status' do
     it 'should return a json response for no rotation job in queue or in progress' do
-      # sidekiq_queue_object = [OpenStruct.new(klass: 'WorkerClass')]
-      # allow(Sidekiq::Queue).to receive(:new).and_return(sidekiq_queue_object)
+      sidekiq_worker_object = []
+      allow(Sidekiq::Workers).to receive(:new).and_return(sidekiq_worker_object)
+
+      sidekiq_queue_object = []
+      allow(Sidekiq::Queue).to receive(:new).and_return(sidekiq_queue_object)
 
       get :rotate_status
 
@@ -13,6 +16,9 @@ RSpec.describe DataEncryptedKeysController, type: :controller do
     end
 
     it 'should return a json response for key rotation job has been queued' do
+      sidekiq_worker_object = []
+      allow(Sidekiq::Workers).to receive(:new).and_return(sidekiq_worker_object)
+
       sidekiq_queue_object = [OpenStruct.new(klass: 'WorkerClass')]
       allow(Sidekiq::Queue).to receive(:new).and_return(sidekiq_queue_object)
 
@@ -20,6 +26,20 @@ RSpec.describe DataEncryptedKeysController, type: :controller do
 
       json_response = JSON.parse(response.body)
       expect(json_response).to eq('message' => 'Key rotation has been queued')
+    end
+
+    it 'should return a json response for key rotation job in progress' do
+      sidekiq_worker_object = double
+      allow(sidekiq_worker_object).to receive(:each).and_yield('process_id', 'thread_id', {'payload' =>  {'queue' => 'default'} })
+      allow(Sidekiq::Workers).to receive(:new).and_return(sidekiq_worker_object)
+
+      sidekiq_queue_object = []
+      allow(Sidekiq::Queue).to receive(:new).and_return(sidekiq_queue_object)
+
+      get :rotate_status
+
+      json_response = JSON.parse(response.body)
+      expect(json_response).to eq('message' => 'Key rotation is in progress')
     end
   end
 end
