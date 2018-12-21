08a58956045ebd78fbbdc4cd3ff52ce4495910fc
jeffli <jeff.chun.yinli@gmail.com>
2018-12-21 15:15:57 -0500

    implemented rotate_key_worker to generate a new data encrypt key, re-encrypt all encrypted strings and delete old data encrypt keys

--- app/workers/rotate_key_worker.rb
+++ app/workers/rotate_key_worker.rb
@@ -2,6 +2,18 @@ class RotateKeyWorker
   include Sidekiq::Worker
 
   def perform
-    DataEncryptingKey.generate!
+    new_key_record = DataEncryptingKey.generate!(primary: true)
+
+    DataEncryptingKey.where("id <> #{new_key_record.id}").update_all(primary: false)
+
+    EncryptedString.all.each do |encrypted_string|
+      value = encrypted_string.value
+      encrypted_string.data_encrypting_key = new_key_record
+      encrypted_string.value = value
+
+      encrypted_string.save!
+    end
+
+    DataEncryptingKey.delete_all(primary: false)
   end
 end
--- spec/workers/rotate_key_worker_spec.rb
+++ spec/workers/rotate_key_worker_spec.rb
@@ -2,10 +2,31 @@ require 'rails_helper'
 
 describe RotateKeyWorker do
   describe '#perform' do
-    it 'should generate a new data encypting key' do
-      expect(DataEncryptingKey.count).to eq(0)
+    it 'should execute the rotate key process' do
+      old_encrypting_key_record = DataEncryptingKey.generate!(primary: true)
+      old_encrypting_key = old_encrypting_key_record.key
+      old_encrypting_key_record_id = old_encrypting_key_record.id
+
+      record_one = EncryptedString.create!(value: 'one')
+      record_two = EncryptedString.create!(value: 'two')
+
+      old_record_one_encrypted_value = record_one.encrypted_value
+      old_record_two_encrypted_value = record_two.encrypted_value
+
       RotateKeyWorker.new.perform
-      expect(DataEncryptingKey.count).to eq(1)
+
+      expect(DataEncryptingKey.primary.key).to_not eq(old_encrypting_key)
+      expect(DataEncryptingKey.find_by(id: old_encrypting_key_record_id)).to be_nil
+
+      updated_record_one = EncryptedString.find(record_one.id)
+
+      expect(updated_record_one.value).to eq('one')
+      expect(updated_record_one.encrypted_value).to_not eq(old_record_one_encrypted_value)
+
+      updated_record_two = EncryptedString.find(record_two.id)
+
+      expect(updated_record_two.value).to eq('two')
+      expect(updated_record_two.encrypted_value).to_not eq(old_record_two_encrypted_value)
     end
   end
 end
