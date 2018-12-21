712f1b5f4dc8c90837b1564955303e2c117c3fde
jeffli <jeff.chun.yinli@gmail.com>
2018-12-21 15:14:27 -0500

    removed incorrect test that does not actually re-encrypt value with a new key

--- spec/models/encrypted_string_spec.rb
+++ spec/models/encrypted_string_spec.rb
@@ -9,21 +9,4 @@ describe EncryptedString do
       expect(record.encrypted_value).to_not eq('test_value')
     end
   end
-
-  describe 'updating existing records' do
-    it 'should utilize another primary data_encrypting_key to re-encrypt an existing value' do
-      first_key = DataEncryptingKey.generate!(primary: true)
-      record = EncryptedString.create!(value: 'test_value')
-      initial_encrypted_value = record.encrypted_value
-      expect(record.value).to eq('test_value')
-      expect(record.encrypted_value).to_not eq('test_value')
-
-      DataEncryptingKey.generate!(primary: true)
-      first_key.update_attribute(:primary, false)
-      record.update_attribute(:value, record.value)
-
-      expect(record.value).to eq('test_value')
-      expect(record.encrypted_value).to_not eq(initial_encrypted_value)
-    end
-  end
 end
