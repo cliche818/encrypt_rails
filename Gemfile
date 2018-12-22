4e64cf6a71c035f6e92eca2bab9b3e0a27907574
jeffli <jeff.chun.yinli@gmail.com>
2018-12-21 20:34:07 -0500

    refactored rotate_key_worker + added sidekiq-limit_fetch to limit the number of running rotate key worker to only 1

--- Gemfile
+++ Gemfile
@@ -25,7 +25,8 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
-gem 'sidekiq'
+gem 'sidekiq', '~> 5.2.0'
+gem 'sidekiq-limit_fetch', '~> 3.4.0'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/workers/rotate_key_worker.rb
+++ app/workers/rotate_key_worker.rb
@@ -1,11 +1,20 @@
 class RotateKeyWorker
   include Sidekiq::Worker
+  sidekiq_options queue: :rotate_key
 
   def perform
     new_key_record = DataEncryptingKey.generate!(primary: true)
 
     DataEncryptingKey.where("id <> #{new_key_record.id}").update_all(primary: false)
 
+    re_encrypt_all_strings(new_key_record)
+
+    DataEncryptingKey.delete_all(primary: false)
+  end
+
+  private
+
+  def re_encrypt_all_strings(new_key_record)
     EncryptedString.all.each do |encrypted_string|
       value = encrypted_string.value
       encrypted_string.data_encrypting_key = new_key_record
@@ -13,7 +22,5 @@ class RotateKeyWorker
 
       encrypted_string.save!
     end
-
-    DataEncryptingKey.delete_all(primary: false)
   end
 end
